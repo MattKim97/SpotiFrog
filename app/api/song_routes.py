758d39f1577779261a9363dfa5bd3a3827ba09c3
Sophia <sophia.tsau@gmail.com>
2023-11-19 19:36:16 -0800

    routes for liking and unliking a song

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -19,7 +19,7 @@ def get_song(id):
     Query for a song by id and returns that song in a dictionary
     """
     song = Song.query.get(id)
-    return song.to_dict(scope="detailed") 
+    return song.to_dict(scope="detailed")
 
 
 @song_routes.route('/new', methods=['POST'])
@@ -34,7 +34,7 @@ def create_song():
     form['csrf_token'].data = request.cookies['csrf_token']
 
     mp3_data = request.files['mp3'].read()
-    
+
 
     if form.validate_on_submit():
         mp3 = form.mp3.data
@@ -43,7 +43,7 @@ def create_song():
 
         if "url" not in upload:
             return upload, 401
-        
+
         new_song = {
             "userId": current_user.id,
             "albumId": form.albumId.data,
@@ -54,14 +54,14 @@ def create_song():
         new_song["playtimeLength"] = analyzePlayTime(mp3_data)
         if new_song["albumId"]:
             album = Album.query.get(new_song["albumId"])
-            new_song["albumTrackNumber"] = len(album.songs) + 1 
+            new_song["albumTrackNumber"] = len(album.songs) + 1
 
         song = Song(**new_song)
         db.session.add(song)
         db.session.commit()
         return song.to_dict(), 201
     elif form.errors:
-        return {"errors": validation_errors_to_error_messages(form.errors)}, 401 
+        return {"errors": validation_errors_to_error_messages(form.errors)}, 401
     else:
         return {"errors": "Unknown error occurred"}, 500
 
@@ -72,7 +72,7 @@ def update_song(id):
     """
     Updates a song and returns the updated song in a dictionary
     """
-    
+
     song = Song.query.get(id)
 
     if song.userId != current_user.id:
@@ -93,10 +93,10 @@ def update_song(id):
         db.session.commit()
         return song.to_dict(), 201
     elif form.errors:
-        return {"errors": validation_errors_to_error_messages(form.errors)}, 401 
+        return {"errors": validation_errors_to_error_messages(form.errors)}, 401
     else:
         return {"errors": "Unknown error occurred"}, 500
-    
+
 @song_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
 def delete_song(id):
@@ -116,3 +116,40 @@ def delete_song(id):
         return {"message": "Song successfully deleted"}
     else:
         return "<h1> File deletion error!<h1>", 401
+
+
+@song_routes.route('/<int:songId>/likes', methods=["POST"])
+@login_required
+def like_song(songId):
+    """
+    Likes a song, creating relationship between user and song
+    """
+    song = Song.query.get(songId)
+
+    if song is None:
+        return {"errors": "Song not found"}, 404
+    elif song in current_user.songLikes:
+        return {"errors": "Cannot like a song that is already liked"}, 401
+    else:
+        current_user.songLikes.append(song)
+        db.session.commit()
+        return {"message": "Song successfully liked"}, 201
+
+
+@song_routes.route('/<int:songId>/likes', methods=["DELETE"])
+@login_required
+def unlike_song(songId):
+    """
+    Unlikes a song, removing relationship between user and song
+    """
+    song = Song.query.get(songId)
+    print("******************SONG*************", song)
+
+    if song is None:
+        return {"errors": "Song not found"}, 404
+    elif song in current_user.songLikes:
+        current_user.songLikes.remove(song)
+        db.session.commit()
+        return {"message": "Song successfully unliked"}, 200
+    else:
+        return {"errors": "Cannot unlike this song"}, 401
