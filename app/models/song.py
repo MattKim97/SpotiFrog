9bb7e09042550d7a9375367f5182a8512e3323a0
bshauck <williamrshauck@gmail.com>
2023-11-21 15:34:06 -0700

    changing table name from playlist_songs back to playlistsSongs to see if Render /sqlAlchemy will handle it.

--- app/models/song.py
+++ app/models/song.py
@@ -6,15 +6,15 @@ from flask_login import UserMixin, current_user
 from datetime import date
 from .user import likes
 
-playlist_songs = db.Table(
-    'playlist_songs',
+playlistsSongs = db.Table(
+    'playlistsSongs',
     db.Model.metadata,
     db.Column('playlistId', db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')), primary_key=True),
     db.Column('songId', db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')), primary_key=True)
 )
 
 if environment == "production":
-    playlist_songs.schema = SCHEMA
+    playlistsSongs.schema = SCHEMA
 
 class Song(db.Model):
     __tablename__ = 'songs'
@@ -51,7 +51,7 @@ class Song(db.Model):
 
     playlist = db.relationship(
         "Playlist",
-        secondary=playlist_songs,
+        secondary=playlistsSongs,
         back_populates="songs",
     )
 
