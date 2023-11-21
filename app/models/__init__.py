ba246ec53b0c87f269f56e2978d931727512b244
bshauck <williamrshauck@gmail.com>
2023-11-21 16:14:10 -0700

    undoing the last change; revert not cooperating

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,5 @@ from .db import db
 from .user import User , likes
 from .db import environment, SCHEMA
 from .album import Album
-from .song import Song, playlistsSongs
+from .song import Song, playlists_songs
 from .playlist import Playlist
