2e4eaf6eccedafc8d8c404b682a0d502ef2e1739
Sophia <sophia.tsau@gmail.com>
2023-11-22 14:40:03 -0800

    added popup modal for removing song from playlist

--- react-app/src/components/Main/RemoveSongFromPlaylist.js
+++ react-app/src/components/Main/RemoveSongFromPlaylist.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useDispatch } from 'react-redux'
 import { thunkRemoveFromPlaylist } from '../../store/playlists'
 
 export default function RemoveSongFromPlaylist({songId, playlistId}) {
     const dispatch = useDispatch()
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = (e) => {
+        e.stopPropagation()
+        e.preventDefault()
+        setIsModalOpen(true)
+    }
+
+    const closeModal = (e) => {
+        e.stopPropagation()
+        e.preventDefault()
+        setIsModalOpen(false)
+    }
 
     async function removeSong(e) {
-        e.preventDefault()
+        e.stopPropagation()
         await dispatch(thunkRemoveFromPlaylist(playlistId, songId))
     }
     return (
-        <button onClick={removeSong}>
+        <>
+        { isModalOpen && (
+            <div className="modal">
+                <div className="modal-content">
+                    <h1>Confirm Delete</h1>
+                    <p>Are you sure you want to remove this song from the playlist?</p>
+                    <div className="modalButtons">
+                        <button className="deleteButton" onClick={removeSong}>
+                            Yes (Remove Song)
+                        </button>
+                        <button className="keepButton" onClick={closeModal}>
+                            No (Keep Song)
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )}
+        <button onClick={openModal}>
             <i className="fa-solid fa-minus"/>
             <span>Remove Song</span>
         </button>
+        </>
     )
 }
