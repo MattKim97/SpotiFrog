e16f356d6e9229e40bee615a361cbd230908c51f
mattk <mkimucsb@gmail.com>
2023-11-27 10:13:39 -0800

    changed songs

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -4,10 +4,10 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="FooterStyle">
-       These ribbiting frogs made this app:
-      <div><a className="FooterLinkStyle" href="https://www.linkedin.com/in/matthew-kim-9ba86a15a/">Matthew Kim  🐸</a></div>
+      <div className="FooterText">These ribbiting frogs made this app:</div>
+      <div><a className="FooterLinkStyle" href="https://www.linkedin.com/in/matthew-kim-9ba86a15a/">Matthew Kim 🐸</a></div>
       <div><a className="FooterLinkStyle" href="https://www.linkedin.com/in/william-shauck-153bb0210/">Bill Shauck 🐸</a></div>
-      <div><a className="FooterLinkStyle" href="https://www.linkedin.com/in/sophia-t-5a51637a/">Sophia Tsau🐸</a></div>
+      <div><a className="FooterLinkStyle" href="https://www.linkedin.com/in/sophia-t-5a51637a/">Sophia Tsau 🐸</a></div>
     </div>
   );
 }
