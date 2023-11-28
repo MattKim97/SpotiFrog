dac1f3d6f282d5dd5c13d211fd00c3dfa0cc3e1b
mattk <mkimucsb@gmail.com>
2023-11-27 17:15:08 -0800

    fixed hard refresh on songs page

--- react-app/src/components/Main/Songs.js
+++ react-app/src/components/Main/Songs.js
@@ -3,16 +3,20 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { thunkGetAllSongs } from "../../store/songs";
 import SongCard from "../SongCard";
+import { thunkGetAllAlbums } from "../../store/albums";
 
 export default function Songs() {
   const dispatch = useDispatch();
   const songs = Object.values(useSelector((state) => state.songs));
+  const albums = Object.values(useSelector((state) => state.albums));
 
   useEffect(() => {
     dispatch(thunkGetAllSongs());
+    dispatch(thunkGetAllAlbums());
   }, [dispatch]);
 
   if (!songs || songs.length === 0) return null;
+  if (!albums || albums.length === 0) return null;
 
   return (
     <div className="ContainerMainStyle">
